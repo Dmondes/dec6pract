6f952b7fb77bdc6531c4dd3ca32c811cce9f559a
Dmondes <desmondlimws@gmail.com>
2024-12-06 15:57:06 +0800

    first task

---
+++ src/main/java/sg/edu/nus/iss/dec6enddaypract/Dec6enddaypractApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.dec6enddaypract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dec6enddaypractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dec6enddaypractApplication.class, args);
+	}
+
+}
